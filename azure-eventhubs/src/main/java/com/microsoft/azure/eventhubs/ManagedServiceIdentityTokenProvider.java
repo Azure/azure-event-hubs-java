35338e926e1651671b7ec59c927e4d5bbd7def0a
Sreeram Garlapati <SreeramGarlapati@users.noreply.github.com>
2017-12-13 18:52:03 -0800

    hook up MSI for functions & App Services (aka antares)

--- azure-eventhubs/src/main/java/com/microsoft/azure/eventhubs/ManagedServiceIdentityTokenProvider.java
+++ azure-eventhubs/src/main/java/com/microsoft/azure/eventhubs/ManagedServiceIdentityTokenProvider.java
@@ -20,12 +20,18 @@ import java.util.concurrent.CompletableFuture;
 
 public class ManagedServiceIdentityTokenProvider implements ITokenProvider {
 
-    static final String METADATA_HEADER_NAME = "Metadata";
-    static final String LOCAL_REST_MSI_ENDPOINT_URL = "http://localhost:50342/oauth2/token";
-    static final String MSI_URL_FORMAT = "%s?resource=%s";
-    static final String MSI_URL = String.format(MSI_URL_FORMAT,
-            LOCAL_REST_MSI_ENDPOINT_URL,
+    static final String LOCAL_VM_REST_MSI_ENDPOINT_URL = "http://localhost:50342/oauth2/token";
+    static final String LOCAL_VM_MSI_URL_FORMAT = "%s?resource=%s";
+    static final String LOCAL_VM_MSI_URL = String.format(LOCAL_VM_MSI_URL_FORMAT,
+            LOCAL_VM_REST_MSI_ENDPOINT_URL,
             AzureActiveDirectoryTokenProvider.EVENTHUBS_REGISTERED_AUDIENCE);
+    static final String METADATA_HEADER_NAME = "Metadata";
+
+    static final String API_VERSION = "api-version=2017-09-01";
+    static final String MSI_SECRET_ENV_VARIABLE = "MSI_SECRET";
+    static final String MSI_ENDPOINT_ENV_VARIABLE = "MSI_ENDPOINT";
+    static final String DYNAMIC_MSI_URL_FORMAT = "%s?resource=%s&%s";
+    static final String SECRET_HEADER_NAME = "Secret";
 
     @Override
     public CompletableFuture<SecurityToken> getToken(
@@ -34,9 +40,23 @@ public class ManagedServiceIdentityTokenProvider implements ITokenProvider {
 
         final CompletableFuture<SecurityToken> getTokenTask = new CompletableFuture<>();
 
+        final String dynamicMsiEndpointUrl = System.getenv(MSI_ENDPOINT_ENV_VARIABLE);
+        final String msiSecret = System.getenv(MSI_SECRET_ENV_VARIABLE);
+
+        final boolean useDynamicMsiUrl;
+
+        if (StringUtil.isNullOrEmpty(dynamicMsiEndpointUrl) || StringUtil.isNullOrEmpty(msiSecret)) {
+            useDynamicMsiUrl = false;
+        }
+        else {
+            useDynamicMsiUrl = true;
+        }
+
         final URL msiUrl;
         try {
-            msiUrl = new URL(MSI_URL);
+            msiUrl = useDynamicMsiUrl
+                    ? new URL(String.format(DYNAMIC_MSI_URL_FORMAT, dynamicMsiEndpointUrl, AzureActiveDirectoryTokenProvider.EVENTHUBS_REGISTERED_AUDIENCE, API_VERSION))
+                    : new URL(LOCAL_VM_MSI_URL);
         } catch (MalformedURLException malformedUrlException) {
             getTokenTask.completeExceptionally(malformedUrlException);
             return getTokenTask;
@@ -50,7 +70,12 @@ public class ManagedServiceIdentityTokenProvider implements ITokenProvider {
             return getTokenTask;
         }
 
-        httpConnection.setRequestProperty(METADATA_HEADER_NAME, "true");
+        if (useDynamicMsiUrl) {
+            httpConnection.setRequestProperty(SECRET_HEADER_NAME, msiSecret);
+        }
+        else {
+            httpConnection.setRequestProperty(METADATA_HEADER_NAME, "true");
+        }
 
         try {
             httpConnection.setRequestMethod("GET");
@@ -71,7 +96,7 @@ public class ManagedServiceIdentityTokenProvider implements ITokenProvider {
         try {
             final StringBuilder responseBuilder = new StringBuilder();
             try (Reader reader = new InputStreamReader(httpConnection.getInputStream(), StandardCharsets.UTF_8)) {
-                char[] buffer = new char[1024];
+                final char[] buffer = new char[1024];
                 int numBytesRead = -1;
                 while ((numBytesRead = reader.read(buffer)) != -1) {
                     responseBuilder.append(buffer, 0, numBytesRead);
