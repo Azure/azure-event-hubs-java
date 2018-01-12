4a5705a9ba44a7d623929112d44d378251a1e2ac
Sreeram Garlapati <SreeramGarlapati@users.noreply.github.com>
2018-01-12 11:11:31 -0800

    test ThreadPool change

--- azure-eventhubs/src/test/java/com/microsoft/azure/eventhubs/exceptioncontracts/MsgFactoryOpenCloseTest.java
+++ azure-eventhubs/src/test/java/com/microsoft/azure/eventhubs/exceptioncontracts/MsgFactoryOpenCloseTest.java
@@ -5,7 +5,11 @@
 package com.microsoft.azure.eventhubs.exceptioncontracts;
 
 import com.microsoft.azure.eventhubs.ConnectionStringBuilder;
+import com.microsoft.azure.eventhubs.EventData;
+import com.microsoft.azure.eventhubs.EventHubClient;
 import com.microsoft.azure.eventhubs.MessagingFactory;
+import com.microsoft.azure.eventhubs.PartitionReceiver;
+import com.microsoft.azure.eventhubs.PartitionSender;
 import com.microsoft.azure.eventhubs.EventHubException;
 import com.microsoft.azure.eventhubs.lib.ApiTestBase;
 import com.microsoft.azure.eventhubs.lib.FaultInjectingReactorFactory;
@@ -15,13 +19,16 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.time.Instant;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class MsgFactoryOpenCloseTest extends ApiTestBase {
 
+    static final String PARTITION_ID = "0";
     static ConnectionStringBuilder connStr;
 
     @BeforeClass
@@ -31,7 +38,63 @@ public class MsgFactoryOpenCloseTest extends ApiTestBase {
     }
 
     @Test()
+    public void VerifyTaskQueueEmptyOnMsgFactoryGracefulClose() throws Exception    {
+
+        final LinkedBlockingQueue<Runnable> blockingQueue = new LinkedBlockingQueue<>();
+        final ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                1, 1, 1, TimeUnit.MINUTES, blockingQueue);
+        try {
+            final EventHubClient ehClient = EventHubClient.createFromConnectionStringSync(
+                    TestContext.getConnectionString().toString(),
+                    executor);
+
+            final PartitionReceiver receiver = ehClient.createReceiverSync(
+                    TestContext.getConsumerGroupName(), PARTITION_ID, Instant.now());
+            final PartitionSender sender = ehClient.createPartitionSenderSync(PARTITION_ID);
+            sender.sendSync(new EventData("test data - string".getBytes()));
+            Iterable<EventData> events = receiver.receiveSync(10);
+
+            Assert.assertTrue(events.iterator().hasNext());
+            sender.closeSync();
+            receiver.closeSync();
+
+            ehClient.closeSync();
+
+            Assert.assertEquals(blockingQueue.size(), 0);
+            Assert.assertEquals(executor.getTaskCount(), executor.getCompletedTaskCount());
+        } finally {
+            executor.shutdown();
+        }
+    }
+
+    @Test()
     public void VerifyThreadReleaseOnMsgFactoryOpenError() throws Exception    {
 
+        final FaultInjectingReactorFactory networkOutageSimulator = new FaultInjectingReactorFactory();
+        networkOutageSimulator.setFaultType(FaultInjectingReactorFactory.FaultType.NetworkOutage);
+
+        final LinkedBlockingQueue<Runnable> blockingQueue = new LinkedBlockingQueue<>();
+        final ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                1, 1, 1, TimeUnit.MINUTES, blockingQueue);
+
+        try {
+            final CompletableFuture<MessagingFactory> openFuture = MessagingFactory.createFromConnectionString(
+                    connStr.toString(), null,
+                    executor,
+                    networkOutageSimulator);
+            try {
+                openFuture.get();
+                Assert.assertFalse(true);
+            } catch (ExecutionException error) {
+                Assert.assertEquals(EventHubException.class, error.getCause().getClass());
+            }
+
+            Thread.sleep(1000); // for reactor to transition from cleanup to complete-stop
+
+            Assert.assertEquals(0, blockingQueue.size());
+            Assert.assertEquals(executor.getTaskCount(), executor.getCompletedTaskCount());
+        } finally {
+            executor.shutdown();
+        }
     }
 }
