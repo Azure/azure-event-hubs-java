95ca2c6d3c2d686c7a49123504d02e00ba36b4eb
Sreeram Garlapati <SreeramGarlapati@users.noreply.github.com>
2018-02-09 16:11:55 -0800

    * skip eph tests which needs azure connection strings * remove partition_count dependency for eventhub tests

--- azure-eventhubs/src/test/java/com/microsoft/azure/eventhubs/sendrecv/SendTest.java
+++ azure-eventhubs/src/test/java/com/microsoft/azure/eventhubs/sendrecv/SendTest.java
@@ -76,7 +76,7 @@ public class SendTest extends ApiTestBase
 	@Test
 	public void sendResultsInSysPropertiesWithPartitionKey() throws EventHubException, InterruptedException, ExecutionException, TimeoutException
 	{
-		final int partitionCount = TestContext.getPartitionCount();
+		final int partitionCount = ehClient.getRuntimeInformation().get().getPartitionCount();
 		final String partitionKey = UUID.randomUUID().toString();
 		CompletableFuture<Void> validateSignal = new CompletableFuture<>();
 		PartitionKeyValidator validator = new PartitionKeyValidator(validateSignal, partitionKey, 1);
@@ -103,7 +103,7 @@ public class SendTest extends ApiTestBase
 	public void sendBatchResultsInSysPropertiesWithPartitionKey() throws EventHubException, InterruptedException, ExecutionException, TimeoutException
 	{
 		final int batchSize = 20;
-		final int partitionCount = TestContext.getPartitionCount();
+		final int partitionCount = ehClient.getRuntimeInformation().get().getPartitionCount();
 		final String partitionKey = UUID.randomUUID().toString();
 		CompletableFuture<Void> validateSignal = new CompletableFuture<>();
 		PartitionKeyValidator validator = new PartitionKeyValidator(validateSignal, partitionKey, batchSize);
